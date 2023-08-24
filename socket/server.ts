6572dfc6cca3552e40697c1ed2e3b56662d54ca8
khs960616 <anjfkrhdkdle@naver.com>
2023-08-24 23:53:39 +0900

    fix: cron 표현식 및 날짜 kst 기준으로 수정

--- socket/server.ts
+++ socket/server.ts
@@ -10,10 +10,19 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.listen(process.env.PORT, ()=>{
-    cron.schedule('0 0 7 * * 1-5', async ()=>{
+    console.log("#######");
+    const date = new Date();
+    date.setHours(date.getHours()+9);
+    console.log(date);
+
+    cron.schedule('0 0 7 * * MON-FRI', async ()=>{
         const sessionValue = await login();
-        const currentDate = new Date().toISOString().split('T')[0].replace(/-/gi, "");
-        const menus: DailyMenu[] = await getDailyLunchMenu(currentDate, sessionValue);
+        const date = new Date();
+        date.setHours(date.getHours()+9);
+        console.log(date);
+
+        const kstDate = date.toISOString().split('T')[0].replace(/-/gi, "");
+        const menus: DailyMenu[] = await getDailyLunchMenu(kstDate, sessionValue);
         sendLunchMenu(menus);
     });
 });
